61a4a0ead7400dc96f6e4f238ab7fd30721f6cdb
Jaynice <janyce.wathuo@student.moringaschool.com>
2025-03-13 12:27:40 +0300

    coded into index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code in this file!
+// Code your solution in this file!
+
+const companyName = "Scuber"; // ✅ Use const
+let mostProfitableNeighborhood = "Chelsea"; // ✅ Use let
+let companyCeo = "Susan Smith"; // ✅ Use let
+
+const currentUser = "Grace Hopper"; // ✅ Define currentUser
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`; // ✅ Template literal
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase(); // ✅ Uppercase message
+
+const shortGreeting = `Welcome, ${currentUser[0]}!`; // ✅ First initial + "!"
+
+
+
